7e368608436a09a16adae1f3e688c27ed3a67ba7
soccercreater <96107026+soccercreater@users.noreply.github.com>
2022-01-11 13:20:55 +0000

    finish bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,10 +16,17 @@ class BooksController < ApplicationController
   end
 
   def update
-    book=Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+     @book=Book.find(params[:id])
+    if @book.update(book_params)
+     flash[:success] = 'Book was successfully created'
+      redirect_to book_path(@book.id)
+    else
+     @books=Book.all
+     render :index
+    end
+
   end
+
 
   def create
     @book = Book.new(book_params)
@@ -28,14 +35,15 @@ class BooksController < ApplicationController
      redirect_to book_path(@book.id)
 
   else
-     render :new
+    @books=Book.all
+     render :index
   end
   end
 
 
   def destroy
-    book=Book.find(params[:id])
-    book.destroy
+    @book=Book.find(params[:id])
+    @book.destroy
     redirect_to books_path
   end
 
